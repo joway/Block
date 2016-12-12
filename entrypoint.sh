f732c4764d39ef520de8f65bd1fd3c63144f84a6
joway <joway.w@gmail.com>
2016-12-12 21:36:43 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-memcached -d -p 11211 -m 256 -c 1024 -l 127.0.0.1
+memcached -d -p 11211 -m 256 -c 1024 -l 127.0.0.1 -u root
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 supervisord -n
