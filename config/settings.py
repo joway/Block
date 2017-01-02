2a17a779975f9070365931471204e287fc0b374f
Joway Wang <joway.w@gmail.com>
2017-01-02 17:53:53 +0800

    change charset

--- config/settings.py
+++ config/settings.py
@@ -176,6 +176,7 @@ else:
             'PASSWORD': os.environ.get("MYSQL_PASSWORD", 'xxx'),
             'HOST': os.environ.get("MYSQL_HOST", 'xxx'),
             'PORT': os.environ.get("MYSQL_PORT", '3306'),
+            'OPTIONS': {'charset': 'utf8mb4'},
         },
     }
 
