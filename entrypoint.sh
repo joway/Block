c7d150c75a0af8c0ebdf1de039bfa2c789e40eb8
Joway Wang <joway.w@gmail.com>
2017-01-07 23:43:32 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,4 @@ python manage.py collectstatic --noinput &
 python manage.py runscript update_qiniu_cache &
 python manage.py migrate --noinput &
 python manage.py update_index &
-celery beat -A config --loglevel=info &
-celery worker -A config --loglevel=info &
 supervisord -n
