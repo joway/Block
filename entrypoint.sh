b3153bcbe213af9db33186ba3cfc061b01cf1efd
Joway Wang <joway.w@gmail.com>
2017-01-11 21:02:41 +0800

    fix popimg bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 nohup sh -c "python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache " &
 python manage.py migrate --noinput
-nohup python manage.py update_index &
+python manage.py update_index
 supervisord -n
