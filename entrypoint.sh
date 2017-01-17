3f66d291554748f547c8ddca6d50509ddcf4edea
Joway Wang <joway.w@gmail.com>
2017-01-17 15:08:50 +0800

    shorting docker start time

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache
-python manage.py migrate --noinput
-python manage.py update_index
 supervisord -n
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache
+python manage.py update_index
