f59b27c6ca91e93bf4ea657d9fbe380e65873da6
Joway Wang <joway.w@gmail.com>
2017-01-17 16:08:01 +0800

    modify topk

--- utils/tag.py
+++ utils/tag.py
@@ -2,4 +2,5 @@ import jieba.analyse
 
 
 def topk(content, k):
-    return jieba.analyse.extract_tags(content, topK=k, withWeight=False, allowPOS=())
+    return jieba.analyse.textrank(content, topK=k, withWeight=False,
+                                  allowPOS=('ns', 'nr', 'an', 'n', 'vn'))
