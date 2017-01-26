93da8dc6fc2f11849a232dfb613e8c861a57c67a
Joway Wang <joway.w@gmail.com>
2017-01-27 00:30:20 +0800

    fix search;

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-supervisord -n
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache
 python manage.py update_index
+supervisord -n
 tail -f /code/log/django.log
