8e6ce0dfd66e9b099a539898041ffff8e5747a0a
Joway Wang <joway.w@gmail.com>
2017-01-28 01:40:16 +0800

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,2 @@
-nohub python manage.py migrate --noinput &
-nohub python manage.py update_index &
-nohub sh -c "python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache" &
+nohub sh -c "python manage.py migrate --noinput && python manage.py update_index && python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache" &
 supervisord -n
