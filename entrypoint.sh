33b48eaf17a78e0952e80ac7dbf3f4516333b76a
Joway Wang <joway.w@gmail.com>
2017-01-27 13:52:49 +0800

    add redis session

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache
 python manage.py update_index
 supervisord -n
+python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache
 tail -f /code/log/django.log
