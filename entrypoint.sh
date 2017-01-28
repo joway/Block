5357a7f4e6eceecffb8a6a1b4c52adc9e7945139
Joway Wang <joway.w@gmail.com>
2017-01-28 15:08:17 +0800

    change crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-nohub sh -c "python manage.py migrate --noinput && python manage.py update_index && python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache" &
+nohup sh -c "python manage.py migrate --noinput && python manage.py update_index && python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache" > /dev/null &
 supervisord -n
