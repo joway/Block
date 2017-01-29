d56819072f6fe0dc4a3b0819ba0074316ddcb507
Joway Wang <joway.w@gmail.com>
2017-01-29 15:27:10 +0800

    cloud dockerfile support;

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-nohup sh -c "python manage.py migrate --noinput && python manage.py update_index && python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache" > /dev/null &
+# nohup sh -c "python manage.py migrate --noinput && python manage.py update_index && python manage.py collectstatic --noinput && python manage.py runscript update_qiniu_cache" > /dev/null &
 supervisord -n
