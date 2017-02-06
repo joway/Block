c1582be81336524310ba14a8a86fb5c93a94d113
Joway Wang <joway.w@gmail.com>
2017-02-06 15:51:56 +0800

    fix nohup

--- articles/apis.py
+++ articles/apis.py
@@ -30,7 +30,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
         ActionService.post(request.user, serializer.instance)
 
-        os.system('nice -19 nohup python manage.py algolia_reindex &')
+        os.system('nohup nice -19 python manage.py algolia_reindex &')
         return Response(self.get_serializer(instance=serializer.instance).data, status=status.HTTP_201_CREATED,
                         headers=headers)
 
@@ -41,7 +41,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
         self.perform_update(serializer)
 
         headers = self.get_success_headers(serializer.data)
-        os.system('nice -19 nohup python manage.py algolia_reindex &')
+        os.system('nohup nice -19 python manage.py algolia_reindex &')
 
         return Response(self.get_serializer(instance=serializer.instance).data, status=status.HTTP_201_CREATED,
                         headers=headers)
