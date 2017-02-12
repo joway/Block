66c61c3522bcd9bde6c1d3f8f5d4ce96854876b0
Joway Wang <joway.w@gmail.com>
2017-02-12 19:23:38 +0800

    fix search

--- articles/apps.py
+++ articles/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 from django.contrib import algoliasearch
+from django.contrib.algoliasearch import AlgoliaIndex
+
+
+class ArticleIndex(AlgoliaIndex):
+    fields = ('title', 'content', 'category')
+    settings = {'attributesToIndex': ['title', 'content']}
+    index_name = 'article_index'
 
 
 class ArticlesConfig(AppConfig):
@@ -8,5 +15,5 @@ class ArticlesConfig(AppConfig):
     def ready(self):
         from actstream import registry
         article = self.get_model('Article')
-        algoliasearch.register(article)
+        algoliasearch.register(article, ArticleIndex)
         registry.register(article)
